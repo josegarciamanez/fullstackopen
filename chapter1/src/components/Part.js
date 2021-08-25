fb0222fc37abc02cd40a57bd7d9c55eb56b36554
Jose García Máñez <josegarciamanez@gmail.com>
2021-08-25 18:24:22 +0200

    finish part 1.2

---
+++ chapter1/src/components/Part.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Part = ({ part, exercise }) => {
+  return (
+    <>
+      <p>
+        {part} {exercise}
+      </p>
+    </>
+  );
+};
+
+export default Part;
